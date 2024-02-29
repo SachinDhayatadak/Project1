c8636d7aa5f62690d784d0733fda29cd5741c2c6
Sachin Dhayatadak <sachindhayatadake@sdlccorp.com>
2024-02-29 17:59:20 +0530

    layout Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,43 @@
 
 import './App.css'
+import {  } from '@heroicons/react/24/solid'
 
 function App() {
 
   return (
-    <div>
-      
-      <span>hi</span>
+    <div className='main-conatiner'>
+      <div className='sidebar-container'>
+        <div className='logo-container'>
+        <h1 className='logo'>coconut</h1>
+        </div>
+        <div>
+
+        </div>
+        
+      </div>
+      <div className='page-container'>
+        <div className='navbar-container'>
+          <div>
+            <span>
+             
+
+
+            </span>
+          </div>
+          <div className='ul-container'>
+            <ul className='ul'>
+              <li className="li-items">Home</li>
+              <li className="li-items">About</li>
+              <li className="li-items">Blog</li>
+              <li className="li-items">Careers</li>
+              <li className="li-items">Login</li>
+            </ul>
+          </div>
+        </div>
+        <div className='body-container'>
+          body
+        </div>
+      </div>
     </div>
   )
 }
